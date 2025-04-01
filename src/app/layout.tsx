13d33a75a593835ae872df75f4653150bc86eda2
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2025-04-01 22:21:17 +0330

    change the font style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";//Poppins
-import {  Poppins  } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
+//import {  Poppins  } from "next/font/google";
 import "../styles/globals.scss";
 
-//const geistSans = Geist({
-//  variable: "--font-geist-sans",
-//  subsets: ["latin"],
-//});
-//
-//const geistMono = Geist_Mono({
-//  variable: "--font-geist-mono",
-//  subsets: ["latin"],
-//});
-
-const poppins =  Poppins({
+const geistSans = Geist({
+  variable: "--font-geist-sans",
   subsets: ["latin"],
-  weight: ['400', '700']
-})
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+
+//const poppins =  Poppins({
+//  subsets: ["latin"],
+//  weight: ['400', '700']
+//})
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -31,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${geistSans.className} antialiased`}
       >
         {children}
       </body>
